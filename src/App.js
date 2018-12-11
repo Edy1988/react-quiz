cb05c96d82d2728e2a4847d3d5c23351129314bd
NaomiBid <naomi0bid@gmail.com>
2018-12-11 11:56:05 +0000

    added question count component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import Question from './components/Question.js';
+import QuestionCount from './components/QuestionCount.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <QuestionCount
+      counter={1}
+      total={5}
+      />
         <Question content="What is your favourite food?" />
       </div>
     );
