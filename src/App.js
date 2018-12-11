81055c508aedcbe14063ce0cc96cc53584f7bea3
NaomiBid <naomi0bid@gmail.com>
2018-12-11 11:40:19 +0000

    displayed question on home page

--- src/App.js
+++ src/App.js
@@ -6,9 +6,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>React Quiz</h2>
-        </div>
         <Question content="What is your favourite food?" />
       </div>
     );
