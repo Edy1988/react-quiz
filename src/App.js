f8e22ff73bd074d287bef627d86849740c7aefb8
NaomiBid <naomi0bid@gmail.com>
2018-12-11 11:38:11 +0000

    resolved file path error for Question.js inside App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React, { Component } from 'react';
+import Question from './components/Question.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          
-          <p>
-          Hi team!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="App-header">
+          <h2>React Quiz</h2>
+        </div>
+        <Question content="What is your favourite food?" />
       </div>
     );
   }
